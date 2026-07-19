f8b793782a17515398ba95d5ed58b96aab992a2a
unknown <erukalachandu30@gmail.com>
2026-07-19 19:40:45 +0530

    Updated main.py

---
+++ withnumbers.py
@@ -0,0 +1,16 @@
+print(3*5)
+print(3*5+7-9/-15*1.5)
+num=15
+print(num/5)
+print(num%5)
+print(str(num)+" is my roll no")
+# without converting num to string we cant print num with string no int and string
+print(abs(-15))
+print(pow(2,3)) # 2^3
+print(max(15,150))
+print(min(15,150))
+print(round(15.15))
+from math import *
+print(floor(3.14))
+print(ceil(3.14))
+print(sqrt(25))
