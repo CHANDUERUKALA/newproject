ff288fe76e6ed7522ffd86199bc3b328ebae5e4c
unknown <erukalachandu30@gmail.com>
2026-07-20 10:07:08 +0530

    updated main

---
+++ basic_Calc.py
@@ -0,0 +1,8 @@
+num1=float(input("Enter a number: "))
+num2=float(input("Enter another number: "))
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2) # int
+print(num1//num2) # its floor
+print(num1%num2)
